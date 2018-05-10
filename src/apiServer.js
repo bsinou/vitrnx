b966df6ac6bfb4e654ddbe115032867f3a6c82ac
Bruno SINOU <bruno.sinou@posteo.de>
2018-05-10 17:25:20 +0200

    Some cleaning before staging

--- src/apiServer.js
+++ src/apiServer.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-//     baseURL: 'https://41.sinou.org/api',
+//    baseURL: 'http://localhost:8888/api',
+     
 
 
 const apiServer = axios.create({
-    baseURL: 'http://localhost:8888/api',
+    baseURL: 'https://41.sinou.org/api',
 });
 
 export default apiServer;
