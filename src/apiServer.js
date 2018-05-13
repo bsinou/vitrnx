6763704c21ff9690d8b21c51a4073d15187600b4
Bruno SINOU <bruno.sinou@posteo.de>
2018-05-13 23:28:39 +0200

    Use correct url

--- src/apiServer.js
+++ src/apiServer.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 //    baseURL: 'http://localhost:8888/api',
-    // baseURL: 'https://41.sinou.org/api',
 
 const apiServer = axios.create({
-    baseURL: 'http://localhost:8888/api',
-
+    baseURL: 'https://41.sinou.org/api',
 });
 
 export default apiServer;
